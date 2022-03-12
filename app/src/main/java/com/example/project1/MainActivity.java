6302acd144ada6a3a25b1385df5740c57c1febf3
OverTheBeez <91337046+OverTheBeez@users.noreply.github.com>
2022-03-12 17:52:17 -0600

    Part 2: Code functionality into buttons.  Calculator works as it should.

--- app/src/main/java/com/example/project1/MainActivity.java
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -3,12 +3,186 @@ package com.example.project1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView answerTextView;
+    Button zeroButton, oneButton, twoButton, threeButton, fourButton, fiveButton, sixButton, sevenButton,
+            eightButton, nineButton, addButton, subtractButton, multiplyButton, divideButton, equalsButton, decimalButton;
+
+    double value1, value2;
+
+    boolean add, subtract, multiply, divide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        answerTextView = (TextView) findViewById(R.id.answerTextView);
+
+        zeroButton = (Button) findViewById(R.id.zeroButton);
+        oneButton = (Button) findViewById(R.id.oneButton);
+        twoButton = (Button) findViewById(R.id.twoButton);
+        threeButton = (Button) findViewById(R.id.threeButton);
+        fourButton = (Button) findViewById(R.id.fourButton);
+        fiveButton = (Button) findViewById(R.id.fiveButton);
+        sixButton = (Button) findViewById(R.id.sixButton);
+        sevenButton = (Button) findViewById(R.id.sevenButton);
+        eightButton = (Button) findViewById(R.id.eightButton);
+        nineButton = (Button) findViewById(R.id.nineButton);
+        addButton = (Button) findViewById(R.id.plusButton);
+        subtractButton = (Button) findViewById(R.id.subtractButton);
+        multiplyButton = (Button) findViewById(R.id.multiplyButton);
+        divideButton = (Button) findViewById(R.id.divisionButton);
+        equalsButton = (Button) findViewById(R.id.equalsButton);
+        decimalButton = (Button) findViewById(R.id.decimalButton);
+
+        zeroButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answerTextView.setText(answerTextView.getText() + "0");
+            }
+        });
+
+        oneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answerTextView.setText(answerTextView.getText() + "1");
+            }
+        });
+
+        twoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answerTextView.setText(answerTextView.getText() + "2");
+            }
+        });
+
+        threeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answerTextView.setText(answerTextView.getText() + "3");
+            }
+        });
+
+        fourButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answerTextView.setText(answerTextView.getText() + "4");
+            }
+        });
+
+        fiveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answerTextView.setText(answerTextView.getText() + "5");
+            }
+        });
+
+        sixButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answerTextView.setText(answerTextView.getText() + "6");
+            }
+        });
+
+        sevenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answerTextView.setText(answerTextView.getText() + "7");
+            }
+        });
+
+        eightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answerTextView.setText(answerTextView.getText() + "8");
+            }
+        });
+
+        nineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answerTextView.setText(answerTextView.getText() + "9");
+            }
+        });
+
+        decimalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answerTextView.setText(answerTextView.getText() + ".");
+            }
+        });
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                value1 = Double.parseDouble(answerTextView.getText() + "");
+                add = true;
+                answerTextView.setText(null);
+
+            }
+        });
+
+        subtractButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                value1 = Double.parseDouble(answerTextView.getText() + "");
+                subtract = true;
+                answerTextView.setText(null);
+
+            }
+        });
+
+        multiplyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                value1 = Double.parseDouble(answerTextView.getText() + "");
+                multiply = true;
+                answerTextView.setText(null);
+
+            }
+        });
+
+        divideButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                value1 = Double.parseDouble(answerTextView.getText() + "");
+                divide = true;
+                answerTextView.setText(null);
+
+            }
+        });
+
+        equalsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                value2 = Double.parseDouble(answerTextView.getText() + "");
+
+                if(add) {
+                    answerTextView.setText(value1 + value2 + "");
+                }
+
+                if(subtract) {
+                    answerTextView.setText(value1 - value2 + "");
+                }
+
+                if(multiply) {
+                    answerTextView.setText(value1 * value2 + "");
+                }
+
+                if(divide) {
+                    answerTextView.setText(value1 / value2 + "");
+                }
+
+            }
+
+        });
+
     }
+
 }
